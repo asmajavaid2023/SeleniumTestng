09a738bbe3e5f635648acd0ea22c38e15837e7bf
asma javaid <asma.javaid@10pearls.com>
2023-06-22 01:00:39 +0500

    new changes

--- src/main/java/Repos/LoginPageRepo.java
+++ src/main/java/Repos/LoginPageRepo.java
@@ -6,13 +6,28 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPageRepo {
 
-    public LoginPageRepo (WebDriver driver1){newDriver=driver1;}
+    public LoginPageRepo (WebDriver driver1){newDriver=driver1;}//class initialize
 
     public WebDriver newDriver;
     public By userName = By.id("user-name");
     public By password = By.id("password");
     public By loginButton = By.id("login-button");
-    public WebElement userNameElement(){return newDriver.findElement(userName);}
+
+    public By forget_password = By.id("forgetid");
+
+    public WebElement get_forget_Element(){
+        return newDriver.findElement(forget_password);
+    }
+
+    public void clickForgetPasswird(){
+        get_forget_Element().click();
+        get_forget_Element().sendKeys();
+    }
+
+
+    public WebElement userNameElement(){
+        return newDriver.findElement(userName);
+    }
     public WebElement passwordElement(){return newDriver.findElement(password);}
     public WebElement loginElement(){return newDriver.findElement(loginButton);}
     public void enterUserNAme(String userName){userNameElement().sendKeys(userName);}
